007c7630b80c8f75b2d1bd9b3614432d57a6dbf6
Vivek Agate <vivek@tenfold.com>
2021-03-28 15:43:51 -0700

    Remove unused file

--- java/maven/example/ServiceVersionReporterExample/src/main/java/com/cgup/examples/svc/VersionReporter.java
+++
@@ -1,8 +0,0 @@
-package com.cgup.examples.svc;
-public class VersionReporter {
-
-    private static final String VERSION="heads/master";
-    public static void reportVersion(){
-        System.out.println("Posting version to VersionViewer: " + VERSION);
-    }
-}
